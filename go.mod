8d0a35b0b103b13e6dafd9b75e4197e2dec206e9
kaka <lokaarziki@gmail.com>
2026-01-08 14:12:08 +0700

    domain property

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.40.0
+	golang.org/x/text v0.27.0
 )
 
 require (
@@ -39,7 +40,6 @@ require (
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.27.0 // indirect
 	golang.org/x/tools v0.34.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
 )
--- internal/domain/property.go
+++ internal/domain/property.go
@@ -1,22 +1,29 @@
 package domain
 
+import "time"
+
 type Property struct {
-	ID          				int     `json:"id"`
-	Title       				string  `json:"title"`
-	Address	    				string  `json:"location"`
-	Province 						string	`json:"province"`
-	City 								string	`json:"city"`
-	District 						string	`json:"district"`
-	SubDistrict 				string	`json:"sub_district"`
-	Description			 		string  `json:"description"`
-	Bedroom							string	`json:"bedroom"`
-	Bathroom						string	`json:"bathroom"`
-	Land_Area						string	`json:"land_area"`
-	Building_Area				string	`json:"building_area"`
-	Electricity					string	`json:"electricity"`
-	Water_Source				string	`json:"water_source"`
-	Certificate					string	`json:"certificate"`
-	Year_Constructed		string	`json:"year_constructed"`
-	Price       				int `json:"price"`
-	Status							int			`json:"status"`
+	ID              int       `json:"id"`
+	Title           string    `json:"title"`
+	Price           int       `json:"price"`
+	Status          int       `json:"status"`
+	Province        string    `json:"province"`
+	Regency         string    `json:"regency"`
+	District        string    `json:"district"`
+	Address         string    `json:"address"`
+	Description     string    `json:"description"`
+	BuildingArea    string    `json:"building_area"`
+	LandArea        string    `json:"land_area"`
+	Electricity     string    `json:"electricity"`
+	WaterSource     string    `json:"water_source"`
+	Bedrooms        string    `json:"bedrooms"`
+	Bathrooms       string    `json:"bathrooms"`
+	Floors          int       `json:"floors"`
+	Garage          int       `json:"garage"`
+	Carport         int       `json:"carport"`
+	Certificate     string    `json:"certificate"`
+	YearConstructed string    `json:"year_constructed"`
+	CreatedAt       time.Time `json:"created_at"`
+	PropertyTypeId  int       `json:"property_type_id"`
+	UserId          int       `json:"user_id"`
 }
